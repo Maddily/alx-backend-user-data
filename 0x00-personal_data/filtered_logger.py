5b5d0e2cb7462e4aac2f534f85dd759eb819862e
Maddie <115709272+Maddily@users.noreply.github.com>
2024-07-31 15:36:33 +0300

    Create a formatter class that builds off logging.Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,35 @@ This is a module for filtering sensitive data from a message.
 """
 
 import re
+import logging
+from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record and applies redaction to sensitive data.
+
+        Args:
+            record (logging.LogRecord): The log record to be formatted.
+
+        Returns:
+            str: The formatted log message with redacted sensitive data.
+        """
+
+        formatted_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            formatted_message, self.SEPARATOR)
 
 
 def filter_datum(fields, redaction, message, separator) -> str:
