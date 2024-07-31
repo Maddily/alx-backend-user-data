f5d841d5d62c11b89d5be349353cda40637a9684
Maddie <115709272+Maddily@users.noreply.github.com>
2024-07-31 18:47:18 +0300

    Type-annotate filter_datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -41,27 +41,13 @@ class RedactingFormatter(logging.Formatter):
                             formatted_message, self.SEPARATOR)
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
-    """
-    Filter sensitive data from a message.
-
-    Args:
-        fields (list): A list of strings representing
-            the fields to be filtered.
-        redaction (str): The string to replace the filtered data with.
-        message (str): The message containing the data to be filtered.
-        separator (str): The separator used to separate
-            the fields and their values.
-
-    Returns:
-        str: The filtered message with sensitive data
-            replaced by the redaction string.
-    """
-
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
+    """ Filter sensitive data from a message. """
     pattrn = r'(' + '|'.join(fields) + r')=([^' + re.escape(separator) + r']+)'
     return re.sub(
-            pattrn, lambda match: f'{match.group(1)}={redaction}', message
-        )
+            pattrn, lambda match: f'{match.group(1)}={redaction}', message)
 
 
 def get_logger() -> logging.Logger:
