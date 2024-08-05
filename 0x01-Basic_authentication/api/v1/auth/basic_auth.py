e6cde9b07d7338e8fdfd8ad56403248a05951e00
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-05 20:14:28 +0300

    Fix over-indentation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -90,31 +90,31 @@ class BasicAuth(Auth):
         return user
 
     def current_user(self, request=None) -> TypeVar('User'):
-            """
-            Retrieves the current user based on the provided request.
-            """
+        """
+        Retrieves the current user based on the provided request.
+        """
 
-            if request is None:
-                return None
+        if request is None:
+            return None
 
-            auth_header = self.authorization_header(request)
-            if auth_header is None:
-                return None
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
 
-            encoded_auth_header = self.extract_base64_authorization_header(
-                auth_header)
-            if encoded_auth_header is None:
-                return None
+        encoded_auth_header = self.extract_base64_authorization_header(
+            auth_header)
+        if encoded_auth_header is None:
+            return None
 
-            decoded_auth_header = self.decode_base64_authorization_header(
-                encoded_auth_header)
-            if decoded_auth_header is None:
-                return None
+        decoded_auth_header = self.decode_base64_authorization_header(
+            encoded_auth_header)
+        if decoded_auth_header is None:
+            return None
 
-            user_email, user_pwd = self.extract_user_credentials(
-                decoded_auth_header)
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_auth_header)
 
-            if user_email is None or user_pwd is None:
-                return None
+        if user_email is None or user_pwd is None:
+            return None
 
-            return self.user_object_from_credentials(user_email, user_pwd)
+        return self.user_object_from_credentials(user_email, user_pwd)
