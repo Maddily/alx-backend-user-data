672e7f32b28d6b78da7c382ef2b21ec2d86e6686
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-05 12:12:44 +0300

    Decode a base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 This module contains the BasicAuth class for basic authentication.
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -26,3 +28,22 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+                self, base64_authorization_header: str) -> str:
+        """
+        Decode a base64-encoded authorization header.
+        """
+
+        if (
+            base64_authorization_header is None
+            or not isinstance(base64_authorization_header, str)
+        ):
+            return None
+
+        try:
+            decoded_s = base64.b64decode(base64_authorization_header,
+                                         validate=True)
+            return decoded_s.decode('utf-8')
+        except binascii.Error:
+            return None
