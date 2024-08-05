c025e0512e8032424e945ac67cbcaff49985030e
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-05 11:43:15 +0300

    Create BasicAuth class for basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains the BasicAuth class for basic authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    This class represents the basic authentication mechanism.
+    It inherits from the Auth class.
+    """
+
+    pass
