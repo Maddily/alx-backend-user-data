fd4efd3e9d0eeb501824b500ded1c2d2e1861d49
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-12 17:56:04 +0300

    Implement POST /users endpoint to register users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,9 +4,11 @@ This module contains a Flask application
 that serves as a user authentication service.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -16,6 +18,22 @@ def welcome():
     """
 
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """
+    Register a new user.
+    """
+
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
