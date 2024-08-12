70718b4441fc6bd3ed0a227417f7fe851689ba3d
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-12 20:08:09 +0300

    Write a method that creates a session for a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,3 +59,17 @@ class Auth:
             return bcrypt.checkpw(str.encode(password), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email) -> str:
+        """
+        Create a session for the user with the given email.
+        """
+
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+
+            return session_id
+        except NoResultFound:
+            return None
