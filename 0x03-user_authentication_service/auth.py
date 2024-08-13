fef743b9737cbcf8dc4fe3d2174040ff24ce5322
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-13 10:03:53 +0300

    Fix annotation for get_user_from_session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -74,7 +75,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User | None:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """
         Retrieve a user object based on the provided session ID.
         """
