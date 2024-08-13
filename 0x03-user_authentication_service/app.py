9f787945a7422075c2df7bd6805ca161fbc01ae5
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-13 11:40:04 +0300

    Implement /profile route to display user info

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,5 +75,20 @@ def logout():
     return redirect(url_for('welcome'))
 
 
+@app.route("/profile", strict_slashes=False)
+def profile():
+    """
+    Retrieves the user's profile information.
+    """
+
+    session_id = request.cookies.get('session_id', None)
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if not user:
+        abort(403)
+
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
