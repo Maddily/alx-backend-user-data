6f13d93a9367ed4f55d43cb71b890b27bdfbd0ce
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-13 09:58:01 +0300

    Implement Auth.get_user_from_session_id method
    The method retrieves a user object based on the provided session ID.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,3 +73,17 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        """
+        Retrieve a user object based on the provided session ID.
+        """
+
+        if not session_id:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
